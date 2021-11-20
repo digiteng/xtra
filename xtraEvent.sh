d3e1652543f31c2bf11703d5952859fc8e1fcaf0
digiteng <digiteng@gmail.com>
2021-11-20 22:24:04 +0300

    Update xtraEvent.sh

--- xtraEvent.sh
+++ xtraEvent.sh
@@ -2,16 +2,21 @@
 # wget -q "--no-check-certificate" https://github.com/digiteng/xtra/raw/main/xtraEvent.sh -O - | /bin/sh
 sleep 1
 wget https://github.com/digiteng/xtra/releases/latest/download/xtraEvent.tar.gz -P /tmp
+echo "Downloading the latest plugin version..."
 sleep 1
 rm -rf /usr/lib/enigma2/python/Plugins/Extensions/xtraEvent
 rm -rf /usr/lib/enigma2/python/Components/Converter/xtra*
 rm -rf /usr/lib/enigma2/python/Components/Renderer/xtra*
+echo "old version is removed..."
 sleep 1
 if [ -f /tmp/xtraEvent.tar.gz ]; then
 	tar -xzf /tmp/xtraEvent.tar.gz -C /
 fi
 sleep 1
 rm -rf /tmp/xtraEvent.tar.gz
+echo "new version installed..."
+sleep 1
+echo "e2 restarting..."
 sleep 1
 killall -9 enigma2
 exit 0
