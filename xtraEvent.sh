4f8eca24cbebf4fdd919ed691195edbb83757d0b
digiteng <digiteng@gmail.com>
2024-07-30 23:10:40 +0300

    Update xtraEvent.sh

--- xtraEvent.sh
+++ xtraEvent.sh
@@ -4,20 +4,20 @@ sleep 1
 wget -q https://github.com/digiteng/xtra/releases/latest/download/xtraEvent.tar.gz -P /tmp
 echo "Downloading the latest plugin version..."
 sleep 1
-rm -rf /usr/lib/enigma2/python/Plugins/Extensions/xtraEvent
-rm -rf /usr/lib/enigma2/python/Components/Converter/xtra*
-rm -rf /usr/lib/enigma2/python/Components/Renderer/xtra*
-rm -rf /usr/share/enigma2/xtra
+#rm -rf /usr/lib/enigma2/python/Plugins/Extensions/xtraEvent
+#rm -rf /usr/lib/enigma2/python/Components/Converter/xtra*
+#rm -rf /usr/lib/enigma2/python/Components/Renderer/xtra*
+#rm -rf /usr/share/enigma2/xtra
 echo "old version is removed..."
 sleep 1
-if [ -f /tmp/xtraEvent.tar.gz ]; then
-	tar -xzf /tmp/xtraEvent.tar.gz -C /
-fi
-sleep 1
-rm -rf /tmp/xtraEvent.tar.gz
+#if [ -f /tmp/xtraEvent.tar.gz ]; then
+#	tar -xzf /tmp/xtraEvent.tar.gz -C /
+#fi
+#sleep 1
+#rm -rf /tmp/xtraEvent.tar.gz
 echo "new version installed..."
 sleep 1
 echo "e2 restarting..."
 sleep 1
-killall -9 enigma2
+#killall -9 enigma2
 exit 0
